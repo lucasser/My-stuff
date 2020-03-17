f1aeb9bc064ec0555b9d4861c4a2b1038cc28de3
Flames <lucserebren@gmail.com>
2020-03-17 12:48:22 -0700

    update app

--- calculator.go
+++ calculator.go
@@ -23,6 +23,42 @@ func divide(n,m int) string {
 	return cs + " R" + ns
         }
 }
+func factorial(n int) int {
+        if n == 1 {
+        	return 1
+        }
+	if n == 0 {
+        	return 1
+        } else {
+        	return n * factorial(n - 1)
+	}
+}
 func main(){
-	fmt.Println(divide(1, 5))
+	for {
+		var o string
+		var a, b int
+
+	  fmt.Printf("Enter an interger: ")
+		fmt.Scanf("%d\n", &a)
+		fmt.Printf("Oh, I got %d\n", a)
+
+		fmt.Printf("Enter another interger: ")
+		fmt.Scanf("%d\n", &b)
+		fmt.Printf("Now I got %d\n", b)
+
+
+		fmt.Println("enter an operation: ")
+		fmt.Scanf("%s\n", &o)
+
+		if o == "f" {
+			fmt.Println(factorial(a))
+		} else {
+			if o == "d" {
+				fmt.Println(divide(a, b))
+			} else {
+				fmt.Printf("why did you give me %s\n", o)
+				fmt.Println("enter f or d for operation")
+			}
+		}
+	}
 }
