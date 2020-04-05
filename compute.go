f5552d51e9631fc50217a5d763a59e28c5e734ed
Flames <lucserebren@gmail.com>
2020-04-05 08:56:07 -0700

    lcm

--- compute.go
+++ compute.go
@@ -106,11 +106,13 @@ func factorial(n uint) uint {
 	}
 }
 
-//func lcm(n, m uint) uint {
-//if m == 1 {
-//return n
-//}
-//if n == 1 {
-//return m
-//}
-//}
+func lcm(n, m uint) uint {
+	if m == 1 {
+		return n
+	}
+	if n == 1 {
+		return m
+	} else {
+		return n * m / gcd(n, m)
+	}
+}
